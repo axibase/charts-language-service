4143ed23c23713bff2e5530a685f550e40bd9863
PolinaShneider <beatrixlinkoln@gmail.com>
2019-08-16 21:06:30 +0300

    Update indents.test.ts

--- src/test/indents.test.ts
+++ src/test/indents.test.ts
@@ -10,7 +10,7 @@ suite("Formatting indents tests: sections and settings", () => {
       `[configuration]
   width-units = 200
   height-units = 200`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "incorrect cfg section",
@@ -18,87 +18,97 @@ suite("Formatting indents tests: sections and settings", () => {
 width-units = 200
   height-units = 200`,
       [TextEdit.replace(Range.create(Position.create(1, 0), Position.create(1, 0)), "  ")],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "correct nested wgt section",
       `[configuration]
   width-units = 200
   height-units = 200
+
   [widget]
     type = chart`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "incorrect nested wgt section",
       `[configuration]
   width-units = 200
   height-units = 200
+
   [widget]
   type = chart`,
-      [TextEdit.replace(Range.create(Position.create(4, 0), Position.create(4, 2)), "    ")],
-      FormattingOptions.create(2, true),
+      [TextEdit.replace(Range.create(Position.create(5, 0), Position.create(5, 2)), "    ")],
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "correct nested series section",
       `[configuration]
   width-units = 200
   height-units = 200
+
   [widget]
     type = chart
+
     [series]
       entity = server`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "incorrect nested series section",
       `[configuration]
   width-units = 200
   height-units = 200
+
   [widget]
     type = chart
+
   [series]
       entity = server`,
-      [TextEdit.replace(Range.create(Position.create(5, 0), Position.create(5, 2)), "    ")],
-      FormattingOptions.create(2, true),
+      [TextEdit.replace(Range.create(Position.create(7, 0), Position.create(7, 2)), "    ")],
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Correct for loop",
       `[configuration]
   width-units = 200
   height-units = 200
+
   [widget]
     type = chart
     for server in servers
+
       [series]
         entity = @{server}
     endfor`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "Incorrect for loop",
       `[configuration]
   width-units = 200
   height-units = 200
+
   [widget]
     type = chart
     for server in servers
+
       [series]
       entity = @{server}
     endfor`,
-      [TextEdit.replace(Range.create(Position.create(7, 0), Position.create(7, "      ".length)), "        ")],
-      FormattingOptions.create(2, true),
+      [TextEdit.replace(Range.create(Position.create(9, 0), Position.create(9, "      ".length)), "        ")],
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Incorrect nested if in for",
       `
   [widget]
     type = chart
-
     list servers = vps,
       vds
     endlist
     for item in servers
+
       [series]
         entity = @{item}
         if @{item} = vps
@@ -114,7 +124,7 @@ metric = cpu_system
         TextEdit.replace(Range.create(Position.create(13, 0), Position.create(13, 0)), "          "),
         TextEdit.replace(Range.create(Position.create(15, 0), Position.create(15, 0)), "          "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Incorrect formatting in the first for, correct in second",
@@ -122,20 +132,19 @@ metric = cpu_system
   [widget]
     type = chart
     metric = cpu_busy
-
     list servers = nurswgvml006,
       nurswgvml007
     endlist
-
     for server in servers
+
 [series]
     entity = @{server}
 
 [series]
     entity = @{server}
     endfor
-
     for server in servers
+
       [series]
         entity = @{server}
         if server == 'nurswgvml007'
@@ -145,53 +154,62 @@ metric = cpu_system
         endif
     endfor`,
       [
-        TextEdit.replace(Range.create(Position.create(10, 0), Position.create(10, 0)), "      "),
-        TextEdit.replace(Range.create(Position.create(11, 0), Position.create(11, "    ".length)), "        "),
-        TextEdit.replace(Range.create(Position.create(13, 0), Position.create(13, 0)), "      "),
-        TextEdit.replace(Range.create(Position.create(14, 0), Position.create(14, "    ".length)), "        "),
+        TextEdit.replace(Range.create(Position.create(9, 0), Position.create(9, 0)), "      "),
+        TextEdit.replace(Range.create(Position.create(10, 0), Position.create(10, "    ".length)), "        "),
+        TextEdit.replace(Range.create(Position.create(12, 0), Position.create(12, 0)), "      "),
+        TextEdit.replace(Range.create(Position.create(13, 0), Position.create(13, "    ".length)), "        "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "A couple of correct groups",
-      `[group]
+      `
+[group]
+
   [widget]
     type = chart
+
     [series]
       entity = vps
       metric = cpu_busy
+
   [widget]
     type = chart
+
     [series]
       entity = vds
       metric = cpu_busy
+
 [group]
+
   [widget]
     type = chart
+
     [series]
       entity = vps
       metric = cpu_busy
+
   [widget]
     type = chart
+
     [series]
       entity = vds
       metric = cpu_busy`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "Correct for after var declaration",
       `
   [widget]
     type = chart
-
     var servers = [ 'vps', 'vds' ]
-
     for item in servers
+
       [series]
         entity = @{item}
         metric = cpu_busy
     endfor`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "Adds a space between setting name and equals sign",
@@ -200,7 +218,7 @@ metric = cpu_system
       [
         TextEdit.insert(Position.create(1, "  entity".length), " "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Align series setting declared after [tags] and empty line",
@@ -208,14 +226,14 @@ metric = cpu_system
     [series]
       entity = server
       metric = cpu_busy
+
       [tags]
         startime = 2018
-
 starttime = 2018`,
       [
-        TextEdit.insert(Position.create(7, 0), "      "),
+        TextEdit.insert(Position.create(7, 0), "        "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Removes an extra space between setting name and equals sign",
@@ -224,7 +242,7 @@ starttime = 2018`,
       [
         TextEdit.replace(Range.create(1, "  entity".length, 1, "  entity  ".length), " "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Adds a space between list name and equals sign",
@@ -233,7 +251,7 @@ starttime = 2018`,
       [
         TextEdit.insert(Position.create(1, "  list entities".length), " "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Removes an extra space between list name and equals sign",
@@ -242,27 +260,30 @@ starttime = 2018`,
       [
         TextEdit.replace(Range.create(1, "  list entities".length, 1, "  list entities  ".length), " "),
       ],
-      FormattingOptions.create(2, true),
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "Does not affect equals signs in setting value",
       `[configuration]
   script = var hello= value()`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "Incorrect [column] after endfor",
-      `
-[configuration]
+      `[configuration]
   entity = a
+
 [group]
+
   [widget]
     type = chart
+
     [column]
       key = value
       label = Count
       list metrics = a, b
       for metric in metrics
+
         [series]
           metric = @{metric}
       endfor
@@ -270,147 +291,170 @@ starttime = 2018`,
        [column]
       key = value
       label = Value`,
-      [TextEdit.replace(Range.create(Position.create(15, 0), Position.create(15, "       ".length)), "    ")],
-      FormattingOptions.create(2, true),
+      [TextEdit.replace(Range.create(Position.create(18, 0), Position.create(18, "       ".length)), "    ")],
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "No ident increase after one-line script",
-      `
-script = if (dialog) widget.hideEmptySeries(false)
+      `script = if (dialog) widget.hideEmptySeries(false)
 column-time = null`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "[series] at the same indent as [tags] in [widget]",
       `
   [widget]
     type = chart
+
     [tags]
       host = *
 
 [series]`,
-      [TextEdit.replace(Range.create(Position.create(6, 0), Position.create(6, 0)), "    ")],
-      FormattingOptions.create(2, true),
+      [TextEdit.replace(Range.create(Position.create(7, 0), Position.create(7, 0)), "    ")],
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "[column] at the same indent as [tags] in [widget]",
-      `
-[configuration]
+      `[configuration]
+
 [group]
+
   [widget]
     type = console
+
     [tags]
       "type" = logger
+
 [column]
       key = level`,
-      [TextEdit.replace(Range.create(Position.create(7, 0), Position.create(7, 0)), "    ")],
-      FormattingOptions.create(2, true),
+      [TextEdit.replace(Range.create(Position.create(10, 0), Position.create(10, 0)), "    ")],
+      FORMATTING_OPTIONS(),
     ),
     new Test(
       "[dropdown] nested to [widget]",
-      `
-[configuration]
+      `[configuration]
+
 [group]
+
   [widget]
     entity = e
     type = chart
+
     [dropdown]
       change-field = metric
       options = javascript: requestEntitiesMetricsOptions('name', 'label')
+
     [series]`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "[series] at the same indent as [column] and [tags]",
-      `
-[configuration]
+      `[configuration]
   entity = a
+
 [group]
+
   [widget]
     type = bar
+
     [column]
       label-format = tags.instance
-    [tags]
-      instance = *
-    [series]
-      metric = collectd.df.df_complex.reserved
-      color = brown`,
-      [], FormattingOptions.create(2, true),
-    ),
-    new Test(
-      "[series] at the same indent as [column] and [tags] (empty line after tags)",
-      `
-[configuration]
-  entity = a
-[group]
-  [widget]
-    type = bar
-    [column]
-      label-format = tags.instance
+
     [tags]
       instance = *
 
     [series]
       metric = collectd.df.df_complex.reserved
       color = brown`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
+    ),
+    new Test(
+      "[series] at the same indent as [column] and [tags] (empty line after tags)",
+      `[configuration]
+  entity = a
+
+[group]
+
+  [widget]
+    type = bar
+
+    [column]
+      label-format = tags.instance
+
+    [tags]
+      instance = *
+
+    [series]
+      metric = collectd.df.df_complex.reserved
+      color = brown`,
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "[widget] after [column] and [series]",
-      `
-[configuration]
+      `[configuration]
   entity = a
+
 [group]
+
   [widget]
     type = bar
+
     [column]
       column-label-format = tags.instance
+
     [series]
       metric = collectd.cpu.cpu.wait
       metric-label = wait
+
   [widget]
     type = chart`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "[column] > [series] > [tags] inside if and for",
       `
 [group]
+
   [widget]
     type = bar
     var sites = getTags("iis.errors", "site", "\${entity}")
     for site in sites
       if site ! = "DefaultWebSite"
+
         [column]
           label = @{site}
+
         [series]
           metric = iis.errors
+
           [tags]
             "type" = locked
             site = @{site}
 
         [series]
           metric = iis.errors
+
           [tags]
             "type" = notfound
             site = @{site}
-
       endif
     endfor`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "Two [option] at the same indent",
       `
     [dropdown]
       change-field = series.metric
+
       [option]
         text = CPU Busy
         value = cpu_busy
+
       [option]
         text = CPU Idle
         value = cpu_idle`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     ),
     new Test(
       "Child inside keywords, parent - outside",
@@ -418,18 +462,19 @@ column-time = null`,
     [column]
       key = value
       label = Disk Busy, %
-
       var f_systems = getTags("nmon.disk_%busy", "id", "\${entity}", "now - 2 * day")
       for id in f_systems
         if id != 'total'
+
           [series]
             label = @{id}
+
             [tags]
               name = id
               value = @{id}
         endif
       endfor`,
-      [], FormattingOptions.create(2, true),
+      [], FORMATTING_OPTIONS(),
     )
   ];
   tests.forEach((test: Test) => { test.formatTest(); }
@@ -488,80 +533,6 @@ suite("Formatting indents tests: >=, <=, >, <", () => {
   test("Correct >=", () => {
     const text = `if a >= b`;
     const options: FormattingOptions = FormattingOptions.create(2, true);
-    const expected: TextEdit[] = [];
-    const formatter = new Formatter(text, options);
-    const actual = formatter.lineByLine();
-    deepStrictEqual(actual, expected);
-  });
-
-  test("Code written in one line", () => {
-    const text = `script
-        window.userFunction = function () {return Math.round(value / 10) * 10;};
-endscript`;
-    const options: FormattingOptions = FORMATTING_OPTIONS();
-    const expected: TextEdit[] = [
-      TextEdit.replace(Range.create(
-        Position.create(1, 0),
-        Position.create(1, 80)),
-        "  window.userFunction = function () {\n    return Math.round(value / 10) * 10;\n  };"
-      )
-    ];
-    const formatter = new Formatter(text, options);
-    const actual = formatter.lineByLine();
-    deepStrictEqual(actual, expected);
-  });
-
-  test("Unformatted code inside script tag in [configuration]", () => {
-    const text = `[configuration]
-  script
-    window.userFunction = function () {
-    return Math.round(value / 10) * 10;
-    };
-  endscript`;
-    const options: FormattingOptions = FORMATTING_OPTIONS();
-    const expected: TextEdit[] = [
-      TextEdit.replace(Range.create(
-        Position.create(2, 0),
-        Position.create(4, 6)),
-        "    window.userFunction = function () {\n      return Math.round(value / 10) * 10;\n    };"
-      )
-    ];
-    const formatter = new Formatter(text, options);
-    const actual = formatter.lineByLine();
-    deepStrictEqual(actual, expected);
-  });
-
-  test("Unformatted code inside script tag in [group]", () => {
-    const text = `
-[group]
-  script
-    window.userFunction = function () {
-    return Math.round(value / 10) * 10;
-    };
-  endscript`;
-    const options: FormattingOptions = FORMATTING_OPTIONS();
-    const expected: TextEdit[] = [
-      TextEdit.replace(Range.create(
-        Position.create(3, 0),
-        Position.create(5, 6)),
-        "    window.userFunction = function () {\n      return Math.round(value / 10) * 10;\n    };"
-      )
-    ];
-    const formatter = new Formatter(text, options);
-    const actual = formatter.lineByLine();
-    deepStrictEqual(actual, expected);
-  });
-
-  test("Correct code that doesn't need formatting", () => {
-    const text = `[configuration]
-
-  [widget]
-    script` +
-      `        window.userFunction = function () {` +
-      + `  return Math.round(value / 10) * 10;` +
-      `};`
-      + `endscript`;
-    const options: FormattingOptions = FORMATTING_OPTIONS();
     const expected: TextEdit[] = [];
     const formatter = new Formatter(text, options);
     const actual = formatter.lineByLine();
