83cf275b4ba88e243968fa547088e87fb4d8943d
Polina Shneider <beatrixlinkoln@gmail.com>
2019-11-01 12:25:30 +0300

    Add more tests

--- src/test/widgetsPosition.test.ts
+++ src/test/widgetsPosition.test.ts
@@ -35,6 +35,14 @@ suite("Widgets position tests", () => {
         deepStrictEqual(actualDiagnostic, expectedDiagnostic, `Config: \n${config}`);
     });
 
+    test("Correct widgets position shorthand", () => {
+        const config = baseConfig("position = 3-3");
+        const validator = new Validator(config);
+        const actualDiagnostic = validator.lineByLine();
+        const expectedDiagnostic = [];
+        deepStrictEqual(actualDiagnostic, expectedDiagnostic, `Config: \n${config}`);
+    });
+
     test("Widget's position overflows grid 10x10", () => {
         const config = baseConfig("position = 10-10, 11-11");
         const validator = new Validator(config);
@@ -43,6 +51,20 @@ suite("Widgets position tests", () => {
             createDiagnostic(
                 createRange(4, 8, 4),
                 "Widget position '10-10, 11-11' overflows grid 4 × 6",
+                DiagnosticSeverity.Warning
+            )
+        ];
+        deepStrictEqual(actualDiagnostic, expectedDiagnostic, `Config: \n${config}`);
+    });
+
+    test("Widget's position (shorthand) overflows grid 10x10", () => {
+        const config = baseConfig("position = 11-11");
+        const validator = new Validator(config);
+        const actualDiagnostic = validator.lineByLine();
+        const expectedDiagnostic = [
+            createDiagnostic(
+                createRange(4, 8, 4),
+                "Widget position '11-11' overflows grid 4 × 6",
                 DiagnosticSeverity.Warning
             )
         ];
